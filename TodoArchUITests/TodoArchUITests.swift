faced91f549ea70b2eee1942d00c162241138c1c
Jason Duffy <jasonsduffy@gmail.com>
2017-10-21 12:27:55 -0400

    delete cruft

--- TodoArchUITests/TodoArchUITests.swift
+++ TodoArchUITests/TodoArchUITests.swift
@@ -1,36 +1,11 @@
-//
-//  TodoArchUITests.swift
-//  TodoArchUITests
-//
-//  Created by Jason Duffy on 10/21/17.
-//  Copyright © 2017 Peapod Multimedia, ltd. All rights reserved.
-//
-
 import XCTest
 
 class TodoArchUITests: XCTestCase {
         
     override func setUp() {
         super.setUp()
-        
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-        
-        // In UI tests it is usually best to stop immediately when a failure occurs.
-        continueAfterFailure = false
-        // UI tests must launch the application that they test. Doing this in setup will make sure it happens for each test method.
-        XCUIApplication().launch()
 
-        // In UI tests it’s important to set the initial state - such as interface orientation - required for your tests before they run. The setUp method is a good place to do this.
+        continueAfterFailure = false
+        XCUIApplication().launch()
     }
-    
-    override func tearDown() {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-        super.tearDown()
-    }
-    
-    func testExample() {
-        // Use recording to get started writing UI tests.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
-    }
-    
 }
